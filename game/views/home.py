edcdc7de2eb7a2f1de5182e523967c897d67088e
雍 正 <yzyoyosir@youmatoMacBook-Air.local>
2012-11-14 15:00:25 +0800

    Issue #16

--- game/views/home.py
+++ game/views/home.py
@@ -10,6 +10,13 @@ def home(request, form=None):
     if request.user.is_authenticated():
         if form is None:
             form = TweetForm()
-        return render_to_response(HOME_PATH, {'form':form}, context_instance=RequestContext(request))
+        user = request.user;
+        follows = Follow.objects.filter(diaos=user.userprofile)
+        all_tweets = []
+        for follow in follows:
+            for tweet in follow.goddess.user.tweet_set.all():
+                all_tweets.append(tweet)
+        all_tweets.sort(key = lambda tweet:tweet.time_posted, reverse = True)
+        return render_to_response(HOME_PATH, {'form':form, 'tweet_set':all_tweets}, context_instance=RequestContext(request))
     form = LogInForm()
     return render_to_response(WELCOME_PATH, {'form':form}, context_instance=RequestContext(request))
